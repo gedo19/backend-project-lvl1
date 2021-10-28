7db117a35deb7510c514f097504bf16c889e788e
gedoteam <eu.MaD19@yandex.ru>
2021-10-28 11:13:42 +0600

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,4 @@ export default () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  return name;
 };
