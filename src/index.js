96e9a9b00a6827a1c2cc1a57259f33d5af880316
gedoteam <eu.MaD19@yandex.ru>
2021-10-28 13:19:05 +0600

    add put games logic to one place

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+
+export default (rule, questAndAnsw) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+
+  console.log(rule);
+
+  const questionsCount = questAndAnsw.length;
+
+  for (let questionNum = 0; questionNum < questionsCount; questionNum += 1) {
+    const question = questAndAnsw[questionNum][0];
+    const correctAnswer = String(questAndAnsw[questionNum][1]);
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer) {
+      return console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer is '${correctAnswer}')`,
+      );
+    }
+
+    console.log('Correct!');
+  }
+
+  return console.log(`Congratulations, ${name}`);
+};
