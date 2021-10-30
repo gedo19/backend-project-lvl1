1e4772e487b44b9484e8f2e76f2f09e48846dd3b
Maxim <eu.MaD19@yandex.ru>
2021-10-30 20:23:18 +0600

    brain-even game fix

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -6,19 +6,15 @@ const isEven = (num) => num % 2 === 0;
 export default () => {
   const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  const genQuestionsAndAnswers = () => {
-    const questionsAndAnswers = [];
+  const questionsAndAnswers = [];
 
-    for (let questionNum = 0; questionNum < questionsCount; questionNum += 1) {
-      const num = getRandomNum(1, 30);
-      const question = num;
-      const isNumEven = isEven(num);
-      const answer = isNumEven ? 'yes' : 'no';
-      questionsAndAnswers.push([question, answer]);
-    }
+  for (let questionNum = 0; questionNum < questionsCount; questionNum += 1) {
+    const num = getRandomNum(1, 30);
+    const question = num;
+    const isNumEven = isEven(num);
+    const answer = isNumEven ? 'yes' : 'no';
+    questionsAndAnswers.push([question, answer]);
+  }
 
-    return questionsAndAnswers;
-  };
-
-  playGame(rule, genQuestionsAndAnswers());
+  playGame(rule, questionsAndAnswers);
 };
