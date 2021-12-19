7209f968f9833940a48e12fbef0df308a37a1944
gedo19 <eu.MaD19@yandex.ru>
2021-12-19 18:24:08 +0600

    linter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const playGame = (rule, questionsAndAnswers) => {
   console.log(`Hello, ${name}!`);
 
   console.log(rule);
-  /* eslint-disable-next-line */
   for (const questionAndAnswer of questionsAndAnswers) {
     const [question, correctAnswer] = questionAndAnswer;
     console.log(`Question: ${question}`);
